3e421950cd57878445da62e6a184ed0efd74a1d4
stamaimer <stamaimer@gmail.com>
2017-07-23 23:14:00 +0800

    477_total_hamming_distance

---
+++ 477_total_hamming_distance.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+"""
+date: 07/23/17
+author: stamaimer
+source: https://leetcode.com/problems/total-hamming-distance/#/description
+difficulty: medium
+"""
+
+
+class Solution(object):
+
+    def totalHammingDistance(self, nums):
+
+        result = 0
+
+        for i, num_a in enumerate(nums):
+
+            for num_b in nums[i + 1:]:
+
+                binary_a = "{0:032b}".format(num_a)
+                binary_b = "{0:032b}".format(num_b)
+
+                result += [int(a) ^ int(b)
+                           for a, b in zip(binary_a, binary_b)].count(1)
+        return result
+
+    def totalHammingDistanceV2(self, nums):
+
+        result = 0
+
+        binaries = ["{0:032b}".format(num) for num in nums]
+
+        for i, binary_a in enumerate(binaries):
+
+            for binary_b in binaries[i + 1:]:
+
+                result += [int(a) ^ int(b) for a, b in zip(binary_a, binary_b)]
+
+        return result
+
+    def totalHammingDistanceV3(self, nums):
+
+        return sum(column.count('1') * column.count('0') for column in zip(*map("{0:032b}".format, nums)))
