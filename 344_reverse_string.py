1139c8583ca3df0b96ad4ac613bfe3180f0f4e17
stamaimer <stamaimer@gmail.com>
2017-07-25 15:20:43 +0800

    344_reverse_string

---
+++ 344_reverse_string.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+date: 07/25/17
+author: stamaimer
+source: https://leetcode.com/problems/reverse-string/#/description
+difficulty: easy
+"""
+
+
+class Solution(object):
+
+    def reverseString(self, s):
+
+        return s[::-1]
+
