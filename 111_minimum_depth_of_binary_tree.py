2de44bb64ce7a029d1c3cb7efbb4119a32ede0c2
stamaimer <stamaimer@gmail.com>
2017-07-26 10:58:09 +0800

    111_minimum_depth_of_binary_tree

---
+++ 111_minimum_depth_of_binary_tree.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""
+date: 07/26/17
+author: stamaimer
+source: https://leetcode.com/problems/minimum-depth-of-binary-tree/#/description
+difficulty: easy
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def pre_order_traverse(self, node, temp, deep=0):
+
+        deep += 1
+
+        if node.left:
+
+            self.pre_order_traverse(node.left, temp, deep)
+
+        if node.right:
+
+            self.pre_order_traverse(node.right, temp, deep)
+
+        if not node.left and not node.right:
+
+            temp.append(deep)
+
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        if root:
+
+            temp = list()
+
+            self.pre_order_traverse(root, temp)
+
+            return min(temp)
+
+        else:
+
+            return 0
+
+
+if __name__ == '__main__':
+
+    node_1 = TreeNode(1)
+
+    node_2 = TreeNode(2)
+
+    node_1.left = node_2
+
+    Solution().minDepth(node_1)
